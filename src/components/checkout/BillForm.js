fe3ae8b8f5af63dec12d7ed6d20f16b9eb9b4b43
Kyle Howell <kylhowl@gmail.com>
2021-01-16 12:34:51 -0500

    rolling ball and made form components

---
+++ src/components/checkout/BillForm.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+const BillForm = ( { billInfo, setBillInfo, formStatus, setKey, isChecked, shipInfo, handleCheckbox } ) => {
+
+    return (
+        <>
+        <p>Billing Address</p>
+          <label>
+            <input
+              type='checkbox'
+              id='same-as-shipping'
+              className='nes-checkbox is-small'
+              onChange={handleCheckbox}
+            />
+            <span id='same-shipping-text'>Same as shipping</span>
+          </label>
+          <div className='nes-field'>
+            <label htmlFor='ckout-bill-add1'>Address 1</label>
+            <input
+              type='text'
+              id='ckout-bill-add1'
+              className='nes-input'
+              placeholder='123 Edgeof'
+              disabled={isChecked}
+              value={isChecked ? shipInfo.add1 : billInfo.add1}
+              required
+              onChange={(e) =>
+                setBillInfo({ ...billInfo, add1: e.target.value })
+              }
+            />
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-bill-add2'>Address 2</label>
+            <input
+              type='text'
+              id='ckout-bill-add2'
+              className='nes-input'
+              placeholder='Unit A, Apt 23, etc...'
+              disabled={isChecked}
+              value={isChecked ? shipInfo.add2 : billInfo.add2}
+              onChange={(e) =>
+                setBillInfo({ ...billInfo, add2: e.target.value })
+              }
+            />
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-bill-city'>City</label>
+            <input
+              type='text'
+              id='ckout-bill-city'
+              className='nes-input'
+              disabled={isChecked}
+              value={isChecked ? shipInfo.city : billInfo.city}
+              required
+              placeholder='Pallet Town'
+              onChange={(e) =>
+                setBillInfo({ ...billInfo, city: e.target.value })
+              }
+            />
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-bill-state'>State</label>
+            <div className='nes-select'>
+              <select
+                required
+                id='ckout-bill-state'
+                disabled={isChecked}
+                value={isChecked ? shipInfo.state : billInfo.state}
+                onChange={(e) =>
+                  setBillInfo({ ...billInfo, state: e.target.value })
+                }
+              >
+                <option value='' disabled hidden>
+                  State...
+                </option>
+                <option value='FL'>FL</option>
+              </select>
+            </div>
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-bill-zip'>Zip</label>
+            <input
+              type='text'
+              id='ckout-bill-zip'
+              className='nes-input'
+              disabled={isChecked}
+              value={isChecked ? shipInfo.zipcode : billInfo.zipcode}
+              required
+              onChange={(e) =>
+                setBillInfo({ ...billInfo, zipcode: e.target.value })
+              }
+              placeholder='12345 or 12345-0000'
+            />
+          </div>
+          <br />
+          <button
+            type='button'
+            className='nes-btn is-primary'
+            onClick={() => setKey('shipping')}
+          >
+            Back
+          </button>{' '}
+          <button
+            type='button'
+            className={
+              formStatus.billing ? 'nes-btn is-disabled' : 'nes-btn is-success'
+            }
+            disabled={formStatus.billing}
+            onClick={() => setKey('CC')}
+          >
+            Next
+          </button>
+        </>
+    )
+}
+
+export default BillForm;
---
+++ src/components/checkout/ContactForm.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const ContactForm = ( { contactInfo, setContactInfo, formStatus, setKey } ) => {
+
+    return (
+        <>
+          <div className='nes-field'>
+            <label htmlFor='ckout-first-name'>First Name</label>
+            <input
+              type='text'
+              id='ckout-first-name'
+              className='nes-input'
+              placeholder='Giovanni'
+              value={contactInfo.firstName}
+              required
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, firstName: e.target.value })
+              }
+            />
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-last-name'>Last Name</label>
+            <input
+              type='text'
+              id='ckout-last-name'
+              className='nes-input'
+              placeholder='Unknown'
+              value={contactInfo.lastName}
+              required
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, lastName: e.target.value })
+              }
+            />
+          </div>
+          <div className='nes-field'>
+            <label htmlFor='ckout-email'>Email address</label>
+            <input
+              type='email'
+              id='ckout-email'
+              className='nes-input'
+              placeholder='Enter email'
+              value={contactInfo.email}
+              required
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, email: e.target.value })
+              }
+            />
+            <small className='form-text'>
+              Order confiramtion and shipping updates will be sent to this email
+            </small>
+          </div>
+          <br />
+          <button
+            type='button'
+            className={
+              formStatus.contact ? 'nes-btn is-disabled' : 'nes-btn is-success'
+            }
+            disabled={formStatus.contact}
+            onClick={() => setKey('shipping')}
+          >
+            Next
+          </button>
+        </>
+    )
+}
+
+export default ContactForm;
