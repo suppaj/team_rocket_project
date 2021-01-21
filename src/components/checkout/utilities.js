68e7d11874e55fc80357e07c41e95fc3cb79dafc
Kyle Howell <kylhowl@gmail.com>
2021-01-21 12:03:38 -0500

    50 states as options in forms

--- src/components/checkout/utilities.js
+++ src/components/checkout/utilities.js
@@ -1,2 +1,61 @@
+import React from 'react';
 
-export
+export function statesOptions() {
+  const stateArray = [
+    'AL',
+    'AK',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'FL',
+    'GA',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'OH',
+    'OK',
+    'OR',
+    'PA',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ];
+  return stateArray.map((state) => (
+    <option key={state} value={state}>
+      {state}
+    </option>
+  ));
+}
