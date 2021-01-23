302d49db63bdc80de5c13dd37f6b82776fcfa9b7
Kyle Howell <kylhowl@gmail.com>
2021-01-23 09:02:26 -0500

    fixed logged in cadd to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -28,18 +28,15 @@ const AddToCart = ({ product, isLoggedIn, cartID, orderAmount, cart, setCart }) 
       localStorage.setItem("cart", JSON.stringify(currCart));
 
       if (noDuplicate) {
+        currCart.push(product);
+        setCart(currCart);
+        localStorage.setItem("cart", JSON.stringify(currCart));
         const results = await addCartItem(
           cartID,
           prod_id,
           cart_quantity,
           price
-        );
-
-        if (results) {
-          currCart.push(product);
-          setCart(currCart);
-          localStorage.setItem("cart", JSON.stringify(currCart));
-        }
+        ); 
       }
     } else {
       currCart.map(async (item) => {
