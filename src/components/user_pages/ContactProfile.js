f6b8ca75320f02c96c30323088f2dd23680fba42
Kyle Howell <kylhowl@gmail.com>
2021-01-27 20:55:00 -0500

    bug fix

--- src/components/user_pages/ContactProfile.js
+++ src/components/user_pages/ContactProfile.js
@@ -10,15 +10,14 @@ const ContactProfile = ({user, setUserProfile, master, setMaster, edit, setEdit}
 
     const handleSave = async (e) => {
         e.preventDefault();
-<<<<<<< HEAD
         if ( user.first_name && user.last_name && user.cust_email) {
             let userUpdate = {...user}
             if ( user.cust_email !== master.cust_email) {
             userUpdate = {...user, emailChange : true }
             }
-            document.getElementById('update-dialog').style.display='block';
+            setShow(true)
             const results = await updateUserContact(userUpdate);
-            document.getElementById('update-dialog').style.display = 'none';
+            setShow(false)
             if (results.message) {
                 console.log(results.message)
                 setMessage('Account with that email already exists, changes canceled.')
@@ -31,26 +30,6 @@ const ContactProfile = ({user, setUserProfile, master, setMaster, edit, setEdit}
                 setMessage('Changes Saved');
             }
         } else {
-=======
-        let userUpdate = {...user}
-        if ( user.cust_email != master.cust_email) {
-           userUpdate = {...user, emailChange : true }
-        }
-        setShow(true);
-        const results = await updateUserContact(userUpdate);
-        setShow(false);
-        if (results.message) {
-            console.log(results.message)
-            setMessage('Account with that email already exists, changes canceled.')
-            setUserProfile(master);
-            setEdit(false);
-        } else {
-            setMaster({...master, ...results});
-            setUserProfile({...master, ...results})
-            setEdit(false);
-            setMessage('Changes Saved');
-        }
->>>>>>> master
         setEdit(false);
         setMessage('Changes canceled, no fields can be blank')
         setUserProfile({...master}) 
