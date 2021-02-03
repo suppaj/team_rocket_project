16c18b1a0f25685b895add140037a660d8c502f7
Suppa <joshsuppa@gmail.com>
2021-02-02 23:48:33 -0500

    reworked empty cart display, its a little sloppy but it works. updated more CSS for checkout

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -12,31 +12,24 @@ const stripePromise = loadStripe(
 const ShoppingCart = ({ cartID, cart, setUser, user, isLoggedIn }) => {
   // uncomment and change onClick of button to handleClick to reinstate
 
-  return (
+  return cart.length ? (
     <>
       <Col md={cart.length ? { span: 7 } : { span: "auto", offset: 1 }}>
-        {cart.length ? (
-          cart.map((order, index) => {
-            return (
-              <CartItemCard
-                key={index}
-                order={order}
-                cart={cart}
-                user={user}
-                setUser={setUser}
-                cart_id={cartID}
-                isLoggedIn={isLoggedIn}
-              />
-            );
-          })
-        ) : (
-          <div className="message-list -left">
-            <div className="nes-balloon from-right align-top">
-              <p>Your cart is empty. Gotta buy them all!</p>
-            </div>
-            <i className="nes-ash align-bottom ash-icon"></i>
-          </div>
-        )}
+        {cart.length
+          ? cart.map((order, index) => {
+              return (
+                <CartItemCard
+                  key={index}
+                  order={order}
+                  cart={cart}
+                  user={user}
+                  setUser={setUser}
+                  cart_id={cartID}
+                  isLoggedIn={isLoggedIn}
+                />
+              );
+            })
+          : ""}
       </Col>
       <Col
         className={
@@ -69,15 +62,31 @@ const ShoppingCart = ({ cartID, cart, setUser, user, isLoggedIn }) => {
             </div>
           </div>
         ) : (
-          <div>
-            <a className="nes-btn is-primary cart-return-btn" href="/">
-              Return to Shopping
-            </a>
-          </div>
+          ""
         )}
       </Col>
     </>
+  ) : (
+    <div className="empty-cart-container nes-container">
+      <div className="message-list -left">
+        <div className="nes-balloon from-right align-top">
+          <p>Your cart is empty. Gotta buy them all!</p>
+        </div>
+        <i className="nes-ash align-bottom ash-icon"></i>
+      </div>
+      <a className="nes-btn is-primary cart-return-btn" href="/">
+        Return to Shopping
+      </a>
+    </div>
   );
 };
 
 export default ShoppingCart;
+
+{
+  /*  */
+}
+
+{
+  /*  */
+}
