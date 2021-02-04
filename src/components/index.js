3bf84741ff9779b6193878c37ce3c93d8daef551
Kyle Howell <kylhowl@gmail.com>
2021-02-03 21:24:35 -0500

    footer design

--- src/components/index.js
+++ src/components/index.js
@@ -34,4 +34,6 @@ export { default as ProfileButton } from './header/ProfileButton';
 export { default as AccountPage } from './user_pages/AccountPage';
 export { default as Header } from './header/Header';
 export { default as Access } from './admin/Access_admin';
+export { default as Footer } from './footer/Footer';
+
  
